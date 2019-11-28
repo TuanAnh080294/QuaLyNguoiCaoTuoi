210647cb53c6cd62a67e943f2463ca9c52dae67c
TuanAnh080294 <tuananh080294@gmail.com>
2019-11-28 16:08:23 +0700

    lay-du-lieu-xuat-index.html

--- controllers/nguoiCaoTuoi.controller.js
+++ controllers/nguoiCaoTuoi.controller.js
@@ -1,17 +1,53 @@
 var db = require('../db');
+const shortid = require('shortid');
 var data = db.get('Sheet1').value();
 
 
-module.exports.index = function (req, res) {
-    var arrData = [];
+var getDate = function () {
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    var NgayKham = month + "/" + date + "/" + year;
+    return NgayKham;
+}
 
-    for(var i=0; i<db.get('Sheet1').size().value(); i++) {
+module.exports.index = function (req, res) {
+    var NgayKham = getDate();
+    var arrData = [];
+    var arrIndex = []
+    var dataIndex = db.get('Sheet1')
+        .filter({
+            NgayKham: NgayKham
+        })
+        .value();
+
+    for (var i = 0; i < db.get('Sheet1').size().value(); i++) {
         arrData.push(data[i]);
     }
-    
-    arrIndex = arrData.sort(function(a, b) {
-        return b.Stt - a.Stt;
-    }).slice(0, 10);
+
+    lengthIndex = db.get('Sheet1')
+        .filter({
+            NgayKham: NgayKham
+        })
+        .size()
+        .value();
+
+    for (var i = 0; i < lengthIndex; i++) {
+        arrIndex.push(dataIndex[i]);
+    }
+
+    arrIndex.sort(function (a, b) {
+        return a._id - b._id;
+    });
+
+    console.log(arrIndex);
+
+    // arrIndex = arrData.sort(function (a, b) {
+    //     return b.Stt - a.Stt;
+    // }).slice(0, 10);
+
+
 
     res.render('index', {
         users: arrIndex,
@@ -20,13 +56,8 @@ module.exports.index = function (req, res) {
 }
 
 module.exports.add = function (req, res) {
-
-    let date_ob = new Date();
-    let date = ("0" + date_ob.getDate()).slice(-2);
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    let year = date_ob.getFullYear();
-    var NgayKham = month + "/" + date + "/" + year;
-
+    var id = shortid.generate();
+    var NgayKham = getDate();
     var length = db.get('Sheet1').size().value();
     var Stt = length + 1;
     var HoTen = req.body.txtTen;
@@ -36,6 +67,7 @@ module.exports.add = function (req, res) {
 
     db.get('Sheet1')
         .push({
+            _id: id,
             Stt: Stt,
             HoTen: HoTen,
             NamSinh: NamSinh,
@@ -47,12 +79,12 @@ module.exports.add = function (req, res) {
     res.redirect('/');
 }
 
+module.exports.search = function () {
+    var HoTen = HTML.getElementsById()
+}
+
 // module.exports.deleteStt = function (req, res) {
 //     // var i = 0;
-//     db.get('Sheet1')
-//         .each(function (item) {
-//             // i = i + 1;
-//             item.NgayKham.delete();
-//         })
-//         .write();
+//     db.unset('Sheet1.Stt')
+//         .write()
 // }
